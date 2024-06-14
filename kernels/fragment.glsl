8f3b33e3e83a2d63a771d9f3da308485619ca71e
maelk3 <mael.karembe@dlr.de>
2024-06-14 19:56:56 +0200

    Update README.md and fix typos

--- kernels/fragment.glsl
+++ kernels/fragment.glsl
@@ -13,14 +13,14 @@ out vec4 fColor;
 
 vec4 color_map[NB_COLORS+1] = {vec4(0.0f,   0.0f,   0.0f,   1.0f),
 			       vec4(0.188f, 0.271f, 0.741f, 1.0f),
-			       vec4(0.055f, 0.569f, 0.824,  1.0f),
+			       vec4(0.055f, 0.569f, 0.824f, 1.0f),
 			       vec4(0.251f, 0.729f, 0.600f, 1.0f),
 			       vec4(0.937f, 0.725f, 0.294f, 1.0f),
 			       vec4(0.976f, 0.984f, 0.051f, 1.0f),
 			       vec4(0.976f, 0.984f, 0.051f, 1.0f)};
 
 void main(void) {
-  float normalized_value = (texture(tex, vec3(texture_coord, 0.0)).r-min)/(max-min);
+  float normalized_value = (texture(tex, vec3(texture_coord, 0.0f)).r-min)/(max-min);
 
   int idx = int(floor(normalized_value*NB_COLORS));
 
